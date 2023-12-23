9d5693f5b4a788062e848e440472794624fe94f2
Narayan Das <workmail.narayan@gmail.com>
2023-12-23 14:32:23 +0530

    jobpage basic structure

--- frontend/src/components/JobListing/MainJobSection.jsx
+++ frontend/src/components/JobListing/MainJobSection.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import Searchbar from "./Searchbar";
 import SideBarFilter from "./SideBarFilter";
+import JobCard from "./JobCard";
 function MainJobSection() {
   return (
     <div className="flex px-5 gap-5 mt-20">
       <div className="filters border rounded-xl w-[30%]">
         <SideBarFilter />
       </div>
-      <div className="jobs bg-red-200 h-32 w-[70%]">{/* <Searchbar /> */}</div>
+      <div className="jobs w-[70%]">
+        <div>
+          <Searchbar />
+        </div>
+        <div>
+          <div className="text-gray-500 font-medium my-3 ml-1.5">
+            <span>250 Jobs results</span>
+          </div>
+          <div>
+            <JobCard />
+            <JobCard />
+            <JobCard />
+            <JobCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
