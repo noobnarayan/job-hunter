57c93b0551f3581e89e6baa8d0bbdc2c2a704218
Narayan Das <workmail.narayan@gmail.com>
2024-01-16 17:05:00 +0530

    fix job model

--- backend/src/models/job.model.js
+++ backend/src/models/job.model.js
@@ -1,30 +1,34 @@
 import mongoose, { Schema } from "mongoose";
 import { User } from './user.model.js';
 
-const jobSchema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    responsibilities: [String],
-    requirements: [String],
-    skills: [String],
-    education: String,
-    experience: String,
-    salaryRange: {
-        from: Number,
-        to: Number
+const jobSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        responsibilities: [String],
+        requirements: [String],
+        skills: [String],
+        education: String,
+        experience: String,
+        salaryRange: {
+            from: Number,
+            to: Number
+        },
+        type: String,
+        location: String,
+        datePosted: { type: Date, default: Date.now },
+        employer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+        shortlistedCandidates: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+        benefits: [String],
+        applicationDeadline: Date,
+        remoteWork: Boolean,
+        travelRequirements: String,
+        additionalRequirements: [String],
     },
-    type: String,
-    location: String,
-    datePosted: { type: Date, default: Date.now },
-    employer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    shortlistedCandidates: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    companyLogo: String,
-    benefits: [String],
-    applicationDeadline: Date,
-    remoteWork: Boolean,
-    travelRequirements: String,
-    additionalRequirements: [String],
-});
+    {
+        timestamps: true
+    }
+);
 
 export const Job = mongoose.model("Job", jobSchema);
